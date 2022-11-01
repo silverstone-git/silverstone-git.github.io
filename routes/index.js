44ebeced2ce32dfade0921ee64950a5a4c07bfb5
Aryan Sidhwani <silverstone965@gmail.com>
2022-11-01 22:40:36 +0530

    - Handled server error by connecting app to fail template in try catch

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/", function(req, res, next) {
-  console.log(req.body);
+
+var mailfail;
 
   function sendMyMail(theForm) {
     var command = fs.readFileSync(mailCommandFilePath, 'utf-8');
     const fullMessage = "Subject: " + theForm.subject + "\n" + theForm.message + "\n ~ " + theForm.email;
     command = command.replace("hemlo", fullMessage);
-    const output = childProcess.execSync(command, { encoding: 'utf-8' });
-    console.log('Output was:\n', output);
+    try {
+      const output = childProcess.execSync(command, { encoding: 'utf-8' });
+    } catch (error) {
+      mailfail = true;
+      res.render("fail", {name: req.body.name, message: "Your message was not received due to a server error, please try DM-ing me on instagram"})
+    }
   }
 
   let formObject = 
@@ -31,7 +36,9 @@ router.post("/", function(req, res, next) {
   }
 
   sendMyMail(formObject);
-  res.render("succ", {name: req.body.name, message: "Your message has been received, I will get back to you shortly"});
+  if(!mailfail) {
+    res.render("succ", {name: req.body.name, message: "Your message has been received, I will get back to you shortly"});
+  }
 })
 
 module.exports = router;
