f1a01c1ed3cb95746e489eafa384afd24da7eafd
Aryan Sidhwani <silverstone965@gmail.com>
2022-11-04 21:30:04 +0530

    - added a nonce to keep bots away - responsively added the mugshot in the greet container - removed subject in the form - made a few improvements in heights and margins in platforms and icons,form resp.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var fs = require('fs')
-var path = require('path')
+var fs = require('fs');
+var path = require('path');
 var router = express.Router();
 var childProcess = require('child_process');
 
-var myStaticPath = path.join(__dirname, "../public")
-var mailCommandFilePath = path.join(myStaticPath, "mailCommand.txt")
+var myStaticPath = path.join(__dirname, "../public");
+var mailCommandFilePath = path.join(myStaticPath, "mailCommand.txt");
+
+var nonce = Math.floor(Math.random()*(10**6)).toString();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {sandwich: nonce});
   //res.render('succ', { name: 'Expresss', message: "Good Morning Deer" });
 });
 
@@ -19,7 +21,7 @@ var mailfail;
 
   function sendMyMail(theForm) {
     var command = fs.readFileSync(mailCommandFilePath, 'utf-8');
-    const fullMessage = "Subject: " + theForm.subject + "\n" + theForm.message + "\n ~ " + theForm.email;
+    const fullMessage = theForm.message + "\n ~ " + theForm.email;
     command = command.replace("hemlo", fullMessage);
     try {
       const output = childProcess.execSync(command, { encoding: 'utf-8' });
@@ -33,10 +35,15 @@ var mailfail;
   {
     message: req.body.message,
     email: req.body._replyto,
-    subject: req.body.Subject
   }
 
-  sendMyMail(formObject);
+  if(req.body.sandwich == nonce) {
+    sendMyMail(formObject);
+  } else {
+    mailfail = true;
+    res.render("fail", {name: req.body.name, message: "Your message was not received due to a form error, please try DM-ing me on instagram"})
+  }
+
   if(!mailfail) {
     res.render("succ", {name: req.body.name, message: "Your message has been received, I will get back to you shortly"});
   }
