f5cd1eee5e9607ecaeadbe5dd0a75848fe69192f
silverstone-git <silverstone965@gmail.com>
2023-09-16 14:32:20 +0530

    New Blue - made the header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-
-  ],
+  content: ["./index.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      syne: ["Syne", "sans-serif"],
+    },
   },
   plugins: [],
-darkMode: 'class',
-}
-
+};
